f49c89b06a1baff0bc767b0890e050f0343b2c62
ArmaanMhate <armaanmhate494@gmail.com>
2021-02-09 16:48:08 +0530

    commited

---
+++ src/homepage.component.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './homepage.styles.css'
+
+const HomePage = () => {
+    return (
+        <div>
+            <div className="homepage">
+                <div className="directory-menu">
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1>HATS</h1>
+                            <span>SHOP</span>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1>JACKETS</h1>
+                            <span>SHOP</span>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1>SNEAKERS</h1>
+                            <span>SHOP</span>
+                        </div>
+                    </div>
+
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1>WOMENS</h1>
+                            <span>SHOP</span>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className="menu-item">
+                        <div className="content">
+                            <h1>MENS</h1>
+                            <span>SHOP</span>
+                        </div>
+                    </div>
+                
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default HomePage
